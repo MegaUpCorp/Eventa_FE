283fb6c30c5039ca13d8938e1a6d09cebf30970d
Nguyễn Lê Phương Nam <henrynguyen1553@gmail.com>
2025-02-05 19:53:40 +0700

    style footer

--- src/layout/User/Footer.tsx
+++ src/layout/User/Footer.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
  const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container-xl'>
+      <div className='border border-solid w-full h-[0.2px]'/>
+      <div className='my-4 flex items-center'>
+        <div className='flex justify-start w-1/2'>
+          <p className='mr-4'>English</p>
+          <p>VietNamese</p>
+        </div>
+        <div className='flex justify-end w-1/2'>
+          <p>Non Copyrighted © 2024 Upload by MegaUp</p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Footer
--- src/pages/User/HomePage.tsx
+++ src/pages/User/HomePage.tsx
@@ -3,7 +3,7 @@
 const HomePage = () =>{
   return (
     <div className="container-xl">
-      
+        
     </div>
   )
 }
