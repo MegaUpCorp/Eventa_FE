5464c45c93a0806c712bbe8301b0cf2f89f76968
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-02-07 22:09:09 +0700

    Auto-focus when rendered

--- src/features/Auth/OtpAuthForm.tsx
+++ src/features/Auth/OtpAuthForm.tsx
@@ -4,18 +4,18 @@ import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from 'src/co
 export const OtpAuthForm = () => {
   return (
     <div className='mx-auto'>
-      <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS}>
+      <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS} autoFocus>
         <div className='flex items-center justify-between gap-3'>
           <InputOTPGroup>
-            <InputOTPSlot index={0} autoFocus className='w-12 h-12' />
-            <InputOTPSlot index={1} className='w-12 h-12' />
-            <InputOTPSlot index={2} className='w-12 h-12' />
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
           </InputOTPGroup>
           <InputOTPSeparator />
           <InputOTPGroup>
-            <InputOTPSlot index={3} className='w-12 h-12' />
-            <InputOTPSlot index={2} className='w-12 h-12' />
-            <InputOTPSlot index={5} className='w-12 h-12' />
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={5} />
           </InputOTPGroup>
         </div>
       </InputOTP>
