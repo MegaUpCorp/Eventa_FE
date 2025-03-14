685f0d036d14577f44aa31e3e592443688fba2a6
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-03-14 10:27:49 +0700

    style: :art: send a blast dialog

--- src/features/Events/EventManagement/Blasts/SendABlast/SendABlastDialog.tsx
+++ src/features/Events/EventManagement/Blasts/SendABlast/SendABlastDialog.tsx
@@ -1,4 +1,4 @@
-import { MessageSquareText } from 'lucide-react'
+import { Mail, MessageSquareText, Send } from 'lucide-react'
 import { Badge } from 'src/components/ui/badge'
 import { Card } from 'src/components/ui/card'
 import {
@@ -9,6 +9,12 @@ import {
   DialogTitle,
   DialogTrigger
 } from 'src/components/ui/dialog'
+import { Form, FormControl, FormField, FormItem, FormLabel } from 'src/components/ui/form'
+import { Input } from 'src/components/ui/input'
+import { useSendABlast } from './useSendABlast'
+import { Textarea } from 'src/components/ui/textarea'
+import { SubmitHandler } from 'react-hook-form'
+import { Button } from 'src/components/ui/button'
 
 interface SendABlastDialogProps {
   trigger?: React.ReactNode
@@ -16,6 +22,13 @@ interface SendABlastDialogProps {
 }
 
 const SendABlastDialog = ({ trigger, asChild = false }: SendABlastDialogProps) => {
+  const { methods } = useSendABlast()
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit: SubmitHandler<any> = (data) => {
+    console.log(data)
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild={asChild} className='w-full'>
@@ -32,14 +45,48 @@ const SendABlastDialog = ({ trigger, asChild = false }: SendABlastDialogProps) =
           </Card>
         )}
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className='flex flex-col'>
+        <div className='p-4 mr-auto rounded-full glass'>
+          <Mail size={24} />
+        </div>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Send Blast</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your account and remove your data from our
-            servers.
+            Guests will receive the blast via email. It will also be shown on the event page.
           </DialogDescription>
         </DialogHeader>
+        <Form {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+            <FormField
+              control={methods.control}
+              name='subject'
+              render={() => (
+                <FormItem>
+                  <FormLabel>Subject (Optional)</FormLabel>
+                  <FormControl>
+                    <Input placeholder='New message in ${eventName}' autoFocus />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={methods.control}
+              name='message'
+              render={() => (
+                <FormItem>
+                  <FormLabel>Subject (Optional)</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder='Share a message with your guests' className='min-h-32 max-h-48' />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type='submit' className='text-[#fff]'>
+              <Send />
+              Send
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
